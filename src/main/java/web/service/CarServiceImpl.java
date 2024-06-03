b62a54f11e942cd00242d05c980ca49a3311881d
Dmitriyq09 <36569062+Dmitriyq09@users.noreply.github.com>
2024-06-03 19:04:31 +0300

    2

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -15,7 +15,7 @@ public class CarServiceImpl implements CarService {
         carLists.add(new Car("Volkswagen Touareg II", "серебристый", 2007));
         carLists.add(new Car("BMW e46 Coupe", "белый", 1999));
         carLists.add(new Car("Land Rover Range Rover Evoque I 6-speed", "белый", 2012));
-        carLists.add(new Car("Peugeot 306", "чёрный", 2017));
+        carLists.add(new Car("Peugeot 306", "крастный", 2017));
         carLists.add(new Car("Mercedes-Benz M-Класс II (W164) 350", "чёрный", 2013));
     }
 
